a82e5a3fb18ce778c4c4266b7c916d81cfe654b2
NguynHau <tanhau7420@gmail.com>
2026-07-19 17:00:04 +0700

    Replace old project with new version

---
+++ galleryData.js
@@ -0,0 +1,5 @@
+window.galleryData = [
+    { src: '/anh_brunei.jpg', alt: 'Student Exchange Brunei', location: 'Bandar Seri Begawan, Brunei' },
+    { src: '/embassy.jpg', alt: 'Embassy Vietnam', location: 'Embassy of Vietnam in Brunei' },
+    { src: '/teamvn.jpg', alt: 'Team Vietnam', location: 'Team Vietnam in Brunei' }
+];
